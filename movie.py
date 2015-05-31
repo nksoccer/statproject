9fb2984a672ce2ec08b195e24ecbf3b07f93c8ca
Noah Koster <nksoccerman@gmail.com>
2015-05-31 17:53:02 -0400

    Finished getting scores for both drama and action movies from Rotten Tomatoes

--- movie.py
+++ movie.py
@@ -1,14 +1,24 @@
 class Movie:
-	'Movie class for IMDB movies'
+	'Movie storage class for IMDB movies'
 	
 	def __init__(self, name, date):
 		self.name = name
 		self.date = date
+		
+	def __init__(self, critic, user):
+		self.critic = critic
+		self.user = user
 		
 	def getName(self):
 		return self.name
 		
 	def getDate(self):
 		return self.date
+		
+	def getCriticScore(self):
+		return self.critic
+	
+	def getUserScore(self):
+		return self.user
 		
 	
--- try.py
+++ try.py
@@ -1,8 +1,9 @@
 import urllib2
+import random
 from movie import Movie 
 from lxml import html
 
-NUM_MOVIES = 200
+NUM_MOVIES = 100
 YEAR_MAX = 5
 
 #get list of movies in form from url at imdb site
@@ -18,10 +19,10 @@ def getListFromIMDB(URL):
 	return movielist
 	
 #get list of movies of size movieListLen from IMDB
-def getMovies(movieListLen):
+def getMovies(movieListLen, type):
 	movies = []
 	for num in range(0, movieListLen/50):
-		root_url = "http://www.imdb.com/search/title?at=0&genres=drama&sort=num_votes"
+		root_url = "http://www.imdb.com/search/title?at=0&genres=%s&sort=num_votes" % type
 		if num == 0:
 			movies += getListFromIMDB(root_url)
 		else:
@@ -35,8 +36,39 @@ def alterMovies(n, movies):
 		if m.getDate() > 2015 - n:
 			newList.append(m)
 	return newList
+
+def getRottenTomatoes(movies):
+	#search movie in rotten tomatoes and get url
+	urlstoget = []
+	for m in movies:
+		url = "http://www.rottentomatoes.com/search/?search=" + m.getName().replace(' ', '+')
+		stream = urllib2.urlopen(url)
+		print stream.geturl()
+		#check for redirect
+		if(stream.geturl() != url):
+			urlstoget.append(stream.geturl())
+			continue
+		content = stream.read()
+		tree = html.fromstring(content)	
+		#TODO: check the url if it is the correct year!! quite a few times this fucks up
+		searchURL = tree.xpath('//ul[@id="movie_results_ul"]/li[1]/div/div/a/@href')[0]
+		urlstoget.append("http://www.rottentomatoes.com" + searchURL)
+	
+	scoreList = []
+	for url in urlstoget:
+		content = urllib2.urlopen(url).read()
+		tree = html.fromstring(content)
+		critic_score = tree.xpath('//div[@class="critic-score meter"]/a/span/span/text()')
+		audience_score = tree.xpath('//div[@class="audience-score meter"]/a/div/div/div/span/text()')
+		m = Movie(int(critic_score), int(audience_score))
+		scoreList.append(m)
+	
+	return scoreList
 		
-movies = getMovies(NUM_MOVIES)
-movies = alterMovies(YEAR_MAX, movies)
-for movie in movies:
-	print movie.getName() + " " + str(movie.getDate())
+movies_drama = alterMovies(YEAR_MAX,  getMovies(NUM_MOVIES, "drama"))
+movies_action = alterMovies(YEAR_MAX,  getMovies(NUM_MOVIES, "action"))
+#randomly select 50 from movie list
+drama_sample = random.sample(movies_drama, 5)
+action_sample = random.sample(movies_action, 5)
+scores_action = getRottenTomatoes(action_sample)
+scores_drama = getRottenTomatoes(drama_sample)
