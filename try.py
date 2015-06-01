971685e600c409d420b91671e2acc58bf88912c1
Noah Koster <nksoccerman@gmail.com>
2015-05-31 21:09:43 -0400

    Removed some exceptions, added n values, means for reach

--- try.py
+++ try.py
@@ -5,7 +5,7 @@ import threading
 from movie import Movie 
 from lxml import html
 
-NUM_MOVIES = 1000
+NUM_MOVIES = 1500
 YEAR_MAX = 5
 pool = ThreadPool(processes=1)
 		
@@ -49,6 +49,13 @@ def alterMovies(n, movies):
 
 def setUrl(movie):
 	url = "http://www.rottentomatoes.com/search/?search=" + movie.getName().replace(' ', '+')
+	#check if non-ascii characters contained - don't bother if its contained
+	try:
+		url.decode('ascii')
+	except Exception:
+		return
+	else: pass
+		
 	stream = urllib2.urlopen(url)
 	print stream.geturl()
 	#check for redirect
@@ -62,7 +69,12 @@ def setUrl(movie):
 	movie.setUrl("http://www.rottentomatoes.com" + searchURL)
 
 def setScores(movie):
-	content = urllib2.urlopen(movie.getUrl()).read()
+	try:
+		content = urllib2.urlopen(movie.getUrl()).read()
+	except Exception:
+		return
+	else: pass
+	
 	tree = html.fromstring(content)
 	critic_score = tree.xpath('//div[@class="critic-score meter"]/a/span/span/text()')
 	audience_score = tree.xpath('//div[@class="audience-score meter"]/a/div/div/div/span/text()')
@@ -93,33 +105,55 @@ def setRottenTomatoes(movies):
 		t.join()
 
 #from paired means turn into set of differences then into mean of difference for each set
-def getMeans(scores):
+def getMeansDiff(scores):
 		means = []
 		for set in scores:
+			totalLen = len(set)
 			totaldiff = 0
 			for m in set:
 				totaldiff += abs(m.getCriticScore() - m.getUserScore())
-			avgdiff = totaldiff / len(set)
+				if(m.getCriticScore()  == 0 or m.getUserScore() == 0): totalLen -= 1 #bad entries with no ratings
+			avgdiff = totaldiff / totalLen
 			means.append(avgdiff)
 		return means
 
-def printData(scores):
+def getMeans(scores):
+	means = []
+	for set in scores:
+		total = len(set)
+		critic_total = 0
+		user_total = 0
+		for m in set:
+			critic_total += m.getCriticScore()
+			user_total += m.getUserScore()
+			if(critic_total == 0 or user_total == 0): total -= 1 #bad entries with no ratings
+		means.append([critic_total/total, user_total/total])
+	return means
+		
+def printData(scores, means, means2):
 	fo = open("data.txt", 'wb')
 	for num in range(0, len(scores)):
-		if(num == 0): fo.write("Action Movies\r\n")
-		if(num == 1): fo.write("Drama Movies\r\n")
+		if(num == 0): fo.write("Action Movies - n = %i\r\n" % len(scores[num]))
+		if(num == 1): fo.write("Drama Movies - n = %i\r\n" % len(scores[num]))
 		for m in scores[num]:
-			fo.write(m.toStr()+ "\r\n")
+			try:
+				fo.write(m.toStr()+ "\r\n")
+			except Exception:
+				continue
+			else: pass
+	fo.write("Means: " + "Action - %s, Drama - %s\r\n" % (means[0], means[1]))
+	fo.write("Action-Critic: %i, Action-User: %i, Drama-Critic: %i, Drama-User: %i" % (means2[0][0], means2[0][1], means2[1][0], means2[1][1]))
 	
 #get list of movies		
 movies_drama = alterMovies(YEAR_MAX,  getMovies(NUM_MOVIES, "drama"))
 movies_action = alterMovies(YEAR_MAX,  getMovies(NUM_MOVIES, "action"))
 #randomly select 50 from movie list
-drama_sample = random.sample(movies_drama, 50)
-action_sample = random.sample(movies_action, 50)
+drama_sample = random.sample(movies_drama, 100)
+action_sample = random.sample(movies_action, 100)
 #get scores from rottentomatoes
 setRottenTomatoes(action_sample)
 setRottenTomatoes(drama_sample)
 #get means
-means = getMeans([action_sample, drama_sample])
-printData([action_sample, drama_sample])
+means = getMeansDiff([action_sample, drama_sample])
+means2 = getMeans([action_sample, drama_sample])
+printData([action_sample, drama_sample], means, means2)
